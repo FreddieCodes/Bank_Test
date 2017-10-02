627f41684b95cc96df4769b9f476346dfbb8730a
Frederick Ofori <FrederickCodes@gmail.com>
2017-10-02 14:46:00 +0100

    Increase and decrease_balance methods now insert an instance of Transaction into an array

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,16 +1,19 @@
 class Bank
 
-  attr_reader :balance
+  attr_reader :balance, :transactions
 
   def initialize
     @balance = 0
+    @transactions =[]
   end
 
-  def increase_balance(amount)
+  def increase_balance(amount, date)
+    @transactions << Transaction.new(amount, date, type = "debit")
     @balance += amount
   end
 
-  def decrease_balance(amount)
+  def decrease_balance(amount, date)
+    @transactions << Transaction.new(amount, date, type = "credit")
     @balance -= amount
   end
 
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -2,18 +2,42 @@ require 'bank'
 
 describe Bank do
   subject(:bank) { described_class.new }
-  it "balance has a default value of 0" do
-    expect(bank.balance).to eq(0)
+  context "Balance" do
+    it "balance has a default value of 0" do
+      expect(bank.balance).to eq(0)
+    end
+
+    it "balance can be increased by a set amount" do
+      bank.increase_balance(10, "11/11/17")
+      expect(bank.balance).to eq(10)
+    end
+
+    it "balance can be decreased by a set amount" do
+      bank.decrease_balance(5, "11/11/17")
+      expect(bank.balance).to eq(-5)
+    end
   end
 
-  it "balance can be increased by a set amount" do
-    subject.increase_balance(10)
-    expect(bank.balance).to eq(10)
+  context "transactions" do
+    it "transactions default is an empty array" do
+      expect(bank.transactions.class).to eq(Array)
+      expect(bank.transactions.length).to eq(0)
+    end
+
+    it "transactions are stored in the transactions array" do
+      bank.increase_balance(10,"11/11/17")
+      expect(bank.transactions.length).to eq(1)
+    end
+
+    it "transactions are stored in the transactions array" do
+      bank.increase_balance(10,"11/11/17")
+      bank.decrease_balance(5, "12/11/17")
+      expect(bank.transactions.length).to eq(2)
+    end
+
   end
 
-  it "balance can be decreased by a set amount" do
-    subject.decrease_balance(5)
-    expect(bank.balance).to eq(-5)
-  end
+
+
 
 end
