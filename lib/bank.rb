5f6235b4e09b145f6f6687bc75596e0b3961f7fa
Frederick Ofori <FrederickCodes@gmail.com>
2017-10-02 12:04:27 +0100

    Bank, decrease_balance and increase_balance methods added

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,7 +1,17 @@
 class Bank
 
-  def balance
-    0
+  attr_reader :balance
+
+  def initialize
+    @balance = 0
   end
-  
+
+  def increase_balance(amount)
+    @balance += amount
+  end
+
+  def decrease_balance(amount)
+    @balance -= amount
+  end
+
 end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -4,4 +4,15 @@ describe Bank do
   it "balance has a default value of 0" do
     expect(subject.balance).to eq(0)
   end
+
+  it "balance can be increased by a set amount" do
+    subject.increase_balance(10)
+    expect(subject.balance).to eq(10)
+  end
+
+  it "balance can be decreased by a set amount" do
+    subject.decrease_balance(5)
+    expect(subject.balance).to eq(-5)
+  end
+
 end
