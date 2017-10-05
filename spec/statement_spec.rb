4cd33b15c4843c959fe37d8f2ebc2044cbd15741
Frederick Ofori <FrederickCodes@gmail.com>
2017-10-05 21:29:12 +0100

    Statement and Tranasaction tests altered

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -2,10 +2,8 @@ require 'statement'
 
 describe Statement do
   before(:each) do
-    transaction1 = instance_double("Transaction", amount: 1000, date: "10/10/202", balance: 1000)
-    transaction2 = instance_double("Transaction", amount: -500, date: "13/10/2012",  balance: 500)
-    allow(transaction1).to receive(:transaction_type).and_return("1000.00 ||")
-    allow(transaction2).to receive(:transaction_type).and_return("|| 500.00")
+    transaction1 = "10/10/2012 || 1000.00 || || 1000.00"
+    transaction2 = "13/10/2012 || || 500.00 || 500.00"
     @statement = described_class.new([transaction1, transaction2])
   end
 
@@ -21,7 +19,7 @@ describe Statement do
 
   it 'can prints out a header followed by multiple transactions' do
     header = 'date || credit || debit || balance'
-    row1 = "10/10/202 || 1000.00 || || 1000.00\n"
+    row1 = "10/10/2012 || 1000.00 || || 1000.00\n"
     row2 = "13/10/2012 || || 500.00 || 500.00\n"
     expect { @statement.print }.to output("#{header}\n#{row1}#{row2}").to_stdout
   end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -12,13 +12,6 @@ describe Transaction do
     expect(transaction.date).to be_truthy
   end
 
-  it 'should set the transaction type to credit if amount above 0' do
-    expect(transaction.transaction_type).to eq("50.00 ||")
-  end
-
-  it 'should set the transaction type to debit if amount below 0' do
-    expect(transaction2.transaction_type).to eq("|| 50.00")
-  end
 
   it 'should have a balance parameter' do
     expect(transaction).to respond_to(:balance)
